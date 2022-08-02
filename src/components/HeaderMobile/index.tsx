8c475f9f300dd5cb3fda08deee83f0bc3c5c3e04
gomestzx <felipegomes2009@gmail.com>
2022-08-02 02:31:41 -0300

    feat/ header mobile and font of the textarea

--- src/components/HeaderMobile/index.tsx
+++ src/components/HeaderMobile/index.tsx
@@ -1,31 +1,82 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import styles from './styles.module.scss';
+import { useState } from 'react';
+import Modal from 'react-modal';
+import Sidebar from '../Sidebar';
+import style from './styles.module.scss';
+
+export function Links() {
+  return (
+    <>
+      <div className={style.links}>
+        <Link href='/tools/neumorphism'>
+          <div className={style.row}>
+            <div className={style.text}>
+              <div className={style.link}>Neumorphism</div>
+            </div>
+          </div>
+        </Link>
+        <Link href='/tools/glassmorphism'>
+          <div className={style.row}>
+            <div className={style.text}>
+              <div className={style.link}>Glassmorphism</div>
+            </div>
+          </div>
+        </Link>
+        <Link href='/tools/claymorphism'>
+          <div className={style.row}>
+            <div className={style.text}>
+              <div className={style.link}>Claymorphism</div>
+            </div>
+          </div>
+        </Link>
+        <Link href='/tools/text-gradient'>
+          <div className={style.row}>
+            <div className={style.text}>
+              <div className={style.link}>Text Gradient</div>
+            </div>
+          </div>
+        </Link>
+        <Link href='/tools/background-gradient'>
+          <div className={style.row}>
+            <div className={style.text}>
+              <div className={style.link}>Background Gradient</div>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+}
 
 export default function HeaderMobile() {
+  const [modal, setModal] = useState<boolean>(false);
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
   return (
-    <header className={styles.header}>
-      <a className={styles.logo}>
-        <Image src='/logo.png' alt='' width={190} height={60} />
-      </a>
-      <input className={styles.menuBtn} type='checkbox' id='menu-btn' />
-      <label className={styles.menuIcon} htmlFor='menu-btn'>
-        <span className={styles.navicon} />
-      </label>
-      <ul className={styles.menu}>
-        <li>
-          <Link href='/tools/neumorphism'>Neumorphism</Link>
-        </li>
-        <li>
-          <Link href='/tools/glassmorphism'>Glassmorphism</Link>
-        </li>
-        <li>
-          <Link href='/tools/claymorphism'>Claymorphism</Link>
-        </li>
-        <li>
-          <Link href='/tools/text-gradient'>Text Gradient</Link>
-        </li>
-      </ul>
-    </header>
+    <div className={style.headerMobile}>
+      <button onClick={() => setModal(true)} className={style.btnOpen}>see tools</button>
+      
+
+      {modal ? (
+        <>
+          <Modal isOpen={modal} className={style.modal}>
+            <button className={style.btnClose} onClick={() => setModal(false)}>X</button>
+            <Links />
+          </Modal>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
