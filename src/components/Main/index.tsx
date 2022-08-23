ff30f070dce973736e64dbae25b3271ee95c0939
gomestzx <felipegomes2009@gmail.com>
2022-08-23 08:41:13 -0300

    add banner product hunt

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -43,62 +43,72 @@ export default function Main() {
   const b = hexToRgb(color)?.b;
   return (
     <>
-    <div className={style.container}>
-      <div className={style.row}>
-        <h1>
-          Build great <br />
-          designs with
-          <br />
-          our tools
-        </h1>
-        <div
-          className={style.content}
-          
-        >
-          <p onClick={() => Redirect()} style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>Try it for free 🚀</p>
-        </div>
-      </div>
-
-      <div className={style.row}>
-        <div className={style.textMobile}>
-          <h2>
-            Build great designs
+      <div className={style.container}>
+        <div className={style.row}>
+          <h1>
+            Build great <br />
+            designs with
             <br />
-            with our tools
-          </h2>
-        </div>
-        <div className={style.mainImg}>
-          <div className={style.background}>
-            <div
-              className={style.container}
-              style={{
-                backdropFilter: `blur(${blur}px)`,
-                WebkitBackdropFilter: `blur(${blur}px)`,
-                width: '300px',
-                height: '320px',
-                borderRadius: 12,
-                backgroundColor: `rgba(${r}, ${g}, ${b}, 0.25)`,
-              }}
-            ></div>
+            our tools
+          </h1>
+          <div className={style.content}>
+            <p
+              onClick={() => Redirect()}
+              style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}
+            >
+              Try it for free 🚀
+            </p>
+            <a
+              href='https://www.producthunt.com/posts/tools4css?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-tools4css'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <img
+                src='https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=356512&theme=light'
+                alt='Tools4CSS - Tools&#0032;to&#0032;help&#0032;developers&#0032;build&#0032;interfaces&#0032;modern&#0032;quickly | Product Hunt'
+              />
+            </a>
           </div>
-          <label>Change the blur 👇</label>
-          <Slider
-            value={blur}
-            onChange={(e, value) => setBlur(value as number)}
-            defaultValue={30}
-            className={style.slider}
-          />
-          <input
-            type='color'
-            onChange={(e) => setColor(e.target.value)}
-            value={color}
-          />
-          <label>Change the color 👆</label>
+        </div>
+
+        <div className={style.row}>
+          <div className={style.textMobile}>
+            <h2>
+              Build great designs
+              <br />
+              with our tools
+            </h2>
+          </div>
+          <div className={style.mainImg}>
+            <div className={style.background}>
+              <div
+                className={style.container}
+                style={{
+                  backdropFilter: `blur(${blur}px)`,
+                  WebkitBackdropFilter: `blur(${blur}px)`,
+                  width: '300px',
+                  height: '320px',
+                  borderRadius: 12,
+                  backgroundColor: `rgba(${r}, ${g}, ${b}, 0.25)`,
+                }}
+              ></div>
+            </div>
+            <label>Change the blur 👇</label>
+            <Slider
+              value={blur}
+              onChange={(e, value) => setBlur(value as number)}
+              defaultValue={30}
+              className={style.slider}
+            />
+            <input
+              type='color'
+              onChange={(e) => setColor(e.target.value)}
+              value={color}
+            />
+            <label>Change the color 👆</label>
+          </div>
         </div>
       </div>
-      
-    </div>
-    
     </>
   );
 }
