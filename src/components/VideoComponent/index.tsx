084d8b604de4c65440e6ccf7c0be2cee974ffd26
gomestzx <felipegomes2009@gmail.com>
2022-10-05 22:48:52 -0300

    update video component

--- src/components/VideoComponent/index.tsx
+++ src/components/VideoComponent/index.tsx
@@ -2,21 +2,13 @@ import Image from 'next/image';
 import React from 'react';
 import styles from './styles.module.scss';
 
-
-
 const VideoComponent = () => {
   return (
     <div className={styles.video}>
       <h1>Preview</h1>
-      <video loop autoPlay id="vid" muted>
-        <source src="/review.mp4" />
+      <video loop autoPlay id='vid' muted>
+        <source src='/review.mp4' />
       </video>
-      <div className={styles.gif}>
-        <Image src='/review.gif' width={1280} height={650} alt='reviewn'/>
-      </div>
-      
-       
-      
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
       />
       <HeaderApp />
       <Main />
+      <VideoComponent />
     </>
   );
 };
