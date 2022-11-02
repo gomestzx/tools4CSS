b49d3d1927d9caf3a2bc74e2c3bb37e82b4ad58f
gomestzx <felipegomes2009@gmail.com>
2022-11-02 19:11:02 -0300

    main page as some tool

--- src/components/Contact/index.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-import React from 'react';
-import styles from './styles.module.scss';
-
-const Contact = () => {
-  return <div className={styles.footer}></div>;
-};
-
-export default Contact;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next';
 import { SEO } from '../components/SEO';
-import Footer from '../components/Footer';
-import HeaderApp from '../components/Header';
-import Main from '../components/Main';
-import VideoComponent from '../components/VideoComponent';
+
+import React, { useEffect } from "react";
+import Router from 'next/router'
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    const {pathname} = Router
+    if(pathname == '/' ){
+        Router.push('/claymorphism')
+    }
+  });
   return (
     <>
       <SEO
@@ -13,10 +18,6 @@ const Home: NextPage = () => {
         title='Tools4CSS'
         description='Free CSS Generator. Our tools are designed to help you streamline your code development with dynamic and intuitive CSS generators.'
       />
-      <HeaderApp />
-      <Main />
-      <VideoComponent />
-      <Footer />
     </>
   );
 };
