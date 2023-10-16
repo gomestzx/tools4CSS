dff689be35c03fe8678445ec0e435921fe0c6f1f
gomestzx <felipematheusdev@gmail.com>
2023-10-16 10:48:42 -0300

    feat: new footer

--- src/components/Sections/Footer/Footer.tsx
+++ src/components/Sections/Footer/Footer.tsx
@@ -6,8 +6,18 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='w-full text-center float-right py-8'>
-            <p>Made with ❤️ by gomestzx</p>
+        <div className='w-full lg:w-4/6 mx-auto text-center flex sm:flex-col md:flex-col lg:flex-row justify-between items-center py-8 text-lg font-medium text-slate-900 flex-wrap gap-10 lg:gap-0'>
+            <div className='w-full lg:w-auto'>
+                <Image src="/logo.png" width={40} height={40} alt='' />
+            </div>
+            
+            <div className='flex justify-around gap-4'>
+                <Link href='/privacity'>Privacy</Link>
+                <Link href='/service-terms'>Service Terms</Link>
+                <Link href='/about'>About</Link>
+            </div>
+            <p>Made with ❤️ <br className='lg:block hidden' /> by gomestzx</p>
+            
         </div>
     )
 }
