c90795ace83cb4e58a36562b1a63c5c60c4e76fc
gomestzx <felipematheusdev@gmail.com>
2024-05-20 06:56:26 -0300

    fix: tooltip

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,57 +1,18 @@
-import React, { ReactNode, useState, useRef, useEffect } from 'react';
-import styles from './Tooltip.module.scss';
+import React, { useState } from "react";
+import styles from "./Tooltip.module.scss";
+import { ITooltip } from "./types";
 
-interface TooltipProps {
-  children: ReactNode;
-  text: string;
-}
-
-const Tooltip: React.FC<TooltipProps> = ({ children, text }) => {
+const Tooltip = ({ children, text }: ITooltip) => {
   const [visible, setVisible] = useState(false);
-  const [tooltipStyles, setTooltipStyles] = useState({});
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleMouseEnter = () => {
-      if (ref.current) {
-        const rect = ref.current.getBoundingClientRect();
-        setTooltipStyles({
-          top: rect.bottom + window.scrollY,
-          left: rect.left + rect.width / 2,
-        });
-        setVisible(true);
-      }
-    };
-
-    const handleMouseLeave = () => {
-      setVisible(false);
-    };
-
-    const currentRef = ref.current;
-    if (currentRef) {
-      currentRef.addEventListener('mouseenter', handleMouseEnter);
-      currentRef.addEventListener('mouseleave', handleMouseLeave);
-    }
-
-    return () => {
-      if (currentRef) {
-        currentRef.removeEventListener('mouseenter', handleMouseEnter);
-        currentRef.removeEventListener('mouseleave', handleMouseLeave);
-      }
-    };
-  }, []);
 
   return (
-    <div className={styles.tooltip} ref={ref}>
+    <div
+      className={styles.tooltipWrapper}
+      onMouseEnter={() => setVisible(true)}
+      onMouseLeave={() => setVisible(false)}
+    >
       {children}
-      {visible && (
-        <div
-          className={`${styles['tooltip-content']} font-EuclidRegular text-sm`}
-          style={tooltipStyles}
-        >
-          {text}
-        </div>
-      )}
+      {visible && <div className={styles.tooltip}>{text}</div>}
     </div>
   );
 };
