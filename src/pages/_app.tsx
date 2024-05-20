f34311ab592f272b87cce2873fbd96ce5ee8d8d9
gomestzx <felipematheusdev@gmail.com>
2024-05-20 08:04:27 -0300

    fix: delete renderProvider import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ControlsProvider } from "../context/ControlsContext";
-import { RenderProvider } from "../context/RenderContext";
 import { FavoriteProvider } from "../context/FavoriteContext";
 import { ThemeProvider } from "../context/ThemeContext";
 import Layout from "../components/Layout";
