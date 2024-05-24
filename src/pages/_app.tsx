8a4e8416b11ed5aaa7a3763a0d84b92e4b752672
gomestzx <felipematheusdev@gmail.com>
2024-05-24 14:33:42 -0300

    feat: add animation with framer motion when change pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,28 @@ import { ControlsProvider } from "../context/ControlsContext";
 import { FavoriteProvider } from "../context/FavoriteContext";
 import { ThemeProvider } from "../context/ThemeContext";
 import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <ThemeProvider>
       <FavoriteProvider>
         <ControlsProvider>
           <Layout>
-            <Component {...pageProps} />
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={router.route}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <Component {...pageProps} />
+              </motion.div>
+            </AnimatePresence>
           </Layout>
         </ControlsProvider>
       </FavoriteProvider>
