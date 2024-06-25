4f3ba942d57fa6570a5fae5272d9d561e6682d31
gomestzx <felipematheusdev@gmail.com>
2024-06-25 10:50:07 -0300

    fix: responsive layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,8 +8,8 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blured  dark:bg-black bg-center bg-no-repeat dark:bg-unset dark:bg-repeat">
-      <div className=" max-w-[100rem]">
+    <div className=" lg:flex items-center justify-center bg-blured  dark:bg-black bg-center bg-no-repeat dark:bg-unset dark:bg-repeat">
+      <div className=" lg:max-w-[100rem]">
         <Navbar />
         {children}
         <Footer />
