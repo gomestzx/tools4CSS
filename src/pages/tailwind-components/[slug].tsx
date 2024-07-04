97c7298f83f7641086772b862f7aed9042126164
gomestzx <felipematheusdev@gmail.com>
2024-07-04 18:38:01 -0300

    feat: add SEO

--- src/pages/tailwind-components/[slug].tsx
+++ src/pages/tailwind-components/[slug].tsx
@@ -12,6 +12,7 @@ import { useFavoriteTool } from '@/hooks/useFavoriteTool';
 import { HiOutlineCheck, HiOutlineClipboard, HiOutlineCodeBracket, HiOutlineComputerDesktop, HiOutlineDevicePhoneMobile, HiOutlineDeviceTablet, HiOutlineEye } from "react-icons/hi2";
 import beautify from 'js-beautify';
 import Sidebar from '@/components/Sidebar/Sidebar';
+import { SEO } from '@/components/SEO';
 
 interface Component {
   fileName: string;
@@ -84,71 +85,73 @@ const TailwindComponentsPage = ({ components }: TailwindComponentsPageProps) => 
 
 
   return (
-    <div className="w-full lg:w-5/6 mx-auto font-medium">
-      <div className="flex flex-row justify-between items-center mx-4 lg:mx-0 cap">
-        <Breadcrumb
-          links={[{ href: "/", label: "Home" }, { href: "/", label: "Tailwind Components" }, { label: Array.isArray(slug) ? slug[0] : (`${slugString?.charAt(0).toUpperCase() + slugString?.slice(1)}` ?? '') }]}
-        />
-        <FavoriteButton
-          isFavorited={isFavorited}
-          handleFavorite={handleFavorite}
-        />
-      </div>
-      <Title
-        customTitleClassname='capitalize'
-        title={`${slug} Tailwind Components`}
-        info="Explore a collection of Tailwind CSS components"
-        customInfoClassname="lg:w-3/4"
-      />
-
-      <div className=" grid grid-cols-12 mt-6">
-        <div className=' col-span-2 pr-2 hidden lg:block'>
-          <Sidebar />
+    <>
+    <SEO title={`${Array.isArray(slug) ? slug[0] : (`${slugString?.charAt(0).toUpperCase() + slugString?.slice(1)}` ?? '')} Tailwind Components`} />
+      <div className="w-full lg:w-5/6 mx-auto font-medium">
+        <div className="flex flex-row justify-between items-center mx-4 lg:mx-0 cap">
+          <Breadcrumb
+            links={[{ href: "/", label: "Home" }, { href: "/", label: "Tailwind Components" }, { label: Array.isArray(slug) ? slug[0] : (`${slugString?.charAt(0).toUpperCase() + slugString?.slice(1)}` ?? '') }]}
+          />
+          <FavoriteButton
+            isFavorited={isFavorited}
+            handleFavorite={handleFavorite}
+          />
         </div>
-        <div className="col-span-12 px-4 lg:col-span-10  lg:px-0">
-          <div className='dark:bg-dark-100 bg-white border rounded-lg dark:border-gray-600 border-slate-300 p-6'>
-            {components.map((component, index) => (
-              <div key={index} className="mb-8 w-full">
-                <div className='flex justify-between items-center mb-4'>
-                  <h1 className='text-white font-GilroyMedium text-lg'>{component.fileName.split(".")[0]}</h1>
-                  <div className='text-gray-400 gap-4 hidden lg:flex'>
-                    <button onClick={() => handleWidthChange(index, '320px')}>
-                      <HiOutlineDevicePhoneMobile size={28} className={`${widths[index] === '320px' ? 'text-blue-500' : ''}`} />
-                    </button>
-                    <button onClick={() => handleWidthChange(index, '768px')}>
-                      <HiOutlineDeviceTablet size={28} className={`${widths[index] === '768px' ? 'text-blue-500' : ''}`} />
-                    </button>
-                    <button onClick={() => handleWidthChange(index, '100%')}>
-                      <HiOutlineComputerDesktop size={28} className={`${widths[index] === '100%' ? 'text-blue-500' : ''}`} />
-                    </button>
-                  </div>
-                  <div className='text-white flex'>
-                    <div className=' flex dark:bg-gray-600 bg-slate-300 p-1 rounded-lg font-raleway'>
-                      <button
-                        className={`flex gap-2 justify-center items-center p-1 w-32 rounded-lg text-base ${activeView[index] === 'preview' ? 'bg-blue-600' : 'dark:bg-gray-600 bg-slate-300 text-black dark:text-white'}`}
-                        onClick={() => toggleView(index, 'preview')}
-                      >
-                        <HiOutlineEye size={20} />  preview
+        <Title
+          customTitleClassname='capitalize'
+          title={`${slug} Tailwind Components`}
+          info="Explore a collection of Tailwind CSS components"
+          customInfoClassname="lg:w-3/4"
+        />
+
+        <div className=" grid grid-cols-12 mt-6">
+          <div className=' col-span-2 pr-2 hidden lg:block'>
+            <Sidebar />
+          </div>
+          <div className="col-span-12 px-4 lg:col-span-10  lg:px-0">
+            <div className='dark:bg-dark-100 bg-white border rounded-lg dark:border-gray-600 border-slate-300 p-6'>
+              {components.map((component, index) => (
+                <div key={index} className="mb-8 w-full">
+                  <div className='flex justify-between items-center mb-4'>
+                    <h1 className='text-white font-GilroyMedium text-lg'>{component.fileName.split(".")[0]}</h1>
+                    <div className='text-gray-400 gap-4 hidden lg:flex'>
+                      <button onClick={() => handleWidthChange(index, '320px')}>
+                        <HiOutlineDevicePhoneMobile size={28} className={`${widths[index] === '320px' ? 'text-blue-500' : ''}`} />
                       </button>
-                      <button
-                        className={`flex gap-2 justify-center items-center p-2 w-32 rounded-lg ${activeView[index] === 'code' ? 'bg-blue-600' : 'dark:bg-gray-600 bg-slate-300 text-black dark:text-white'}`}
-                        onClick={() => toggleView(index, 'code')}
-                      >
-                        <HiOutlineCodeBracket size={20} />
-                        code
+                      <button onClick={() => handleWidthChange(index, '768px')}>
+                        <HiOutlineDeviceTablet size={28} className={`${widths[index] === '768px' ? 'text-blue-500' : ''}`} />
+                      </button>
+                      <button onClick={() => handleWidthChange(index, '100%')}>
+                        <HiOutlineComputerDesktop size={28} className={`${widths[index] === '100%' ? 'text-blue-500' : ''}`} />
                       </button>
                     </div>
-                    <button className='ml-6 flex gap-2 justify-center items-center' onClick={() => handleCopy(index)}>
-                      {copiedIndex === index ? <HiOutlineCheck className="text-green-400" size={20} /> : <HiOutlineClipboard className=' text-black dark:text-white' size={20} />}
-                    </button>
+                    <div className='text-white flex'>
+                      <div className=' flex dark:bg-gray-600 bg-slate-300 p-1 rounded-lg font-raleway'>
+                        <button
+                          className={`flex gap-2 justify-center items-center p-1 w-32 rounded-lg text-base ${activeView[index] === 'preview' ? 'bg-blue-600' : 'dark:bg-gray-600 bg-slate-300 text-black dark:text-white'}`}
+                          onClick={() => toggleView(index, 'preview')}
+                        >
+                          <HiOutlineEye size={20} />  preview
+                        </button>
+                        <button
+                          className={`flex gap-2 justify-center items-center p-2 w-32 rounded-lg ${activeView[index] === 'code' ? 'bg-blue-600' : 'dark:bg-gray-600 bg-slate-300 text-black dark:text-white'}`}
+                          onClick={() => toggleView(index, 'code')}
+                        >
+                          <HiOutlineCodeBracket size={20} />
+                          code
+                        </button>
+                      </div>
+                      <button className='ml-6 flex gap-2 justify-center items-center' onClick={() => handleCopy(index)}>
+                        {copiedIndex === index ? <HiOutlineCheck className="text-green-400" size={20} /> : <HiOutlineClipboard className=' text-black dark:text-white' size={20} />}
+                      </button>
+                    </div>
                   </div>
-                </div>
-                {activeView[index] === 'preview' ? (
-                  <iframe
-                    className={`w-full rounded-md ring-2 dark:ring-gray-900 ring-gray-200 lg:transition-all bg-white ${slug === 'navbar' ? 'h-[200px]' : 'h-[500px]'} `}
-                    loading="lazy"
-                    style={{ maxWidth: widths[index] }}
-                    srcDoc={`
+                  {activeView[index] === 'preview' ? (
+                    <iframe
+                      className={`w-full rounded-md ring-2 dark:ring-gray-900 ring-gray-200 lg:transition-all bg-white ${slug === 'navbar' ? 'h-[200px]' : 'h-[500px]'} `}
+                      loading="lazy"
+                      style={{ maxWidth: widths[index] }}
+                      srcDoc={`
                       <!DOCTYPE html>
                       <html class="relative" dir="ltr">
                         <head>
@@ -196,29 +199,30 @@ const TailwindComponentsPage = ({ components }: TailwindComponentsPageProps) => 
                         </body>
                       </html>
                     `}
-                    title={`Component in ${slug}`}
-                  />
-                ) : (
-                  <Highlight className="html">
-                    <div className='h-[300px]'>
-                      {formatHtml(extractReturnContent(component.content))}
-                    </div>
-                  </Highlight>
-                )}
-              </div>
-            ))}
+                      title={`Component in ${slug}`}
+                    />
+                  ) : (
+                    <Highlight className="html">
+                      <div className='h-[300px]'>
+                        {formatHtml(extractReturnContent(component.content))}
+                      </div>
+                    </Highlight>
+                  )}
+                </div>
+              ))}
+            </div>
+
+
+
           </div>
 
-
-
         </div>
-
+        <Info
+          title="What is Tailwind Components?"
+          paragraph="Tailwind Components is a dedicated page that offers a collection of pre-designed Tailwind CSS components available for free. This resource allows designers and developers to easily copy and integrate these components into their projects, ensuring a faster and more efficient workflow. By providing a variety of ready-to-use components, Tailwind Components simplifies the process of creating responsive and visually appealing web pages. Each component is crafted to adhere to best practices, ensuring both functionality and aesthetic appeal. This tool is invaluable for professionals seeking to streamline their web development process and maintain high standards in their design work."
+        />
       </div>
-      <Info
-        title="What is Tailwind Components?"
-        paragraph="Tailwind Components is a dedicated page that offers a collection of pre-designed Tailwind CSS components available for free. This resource allows designers and developers to easily copy and integrate these components into their projects, ensuring a faster and more efficient workflow. By providing a variety of ready-to-use components, Tailwind Components simplifies the process of creating responsive and visually appealing web pages. Each component is crafted to adhere to best practices, ensuring both functionality and aesthetic appeal. This tool is invaluable for professionals seeking to streamline their web development process and maintain high standards in their design work."
-      />
-    </div>
+    </>
   );
 };
 
