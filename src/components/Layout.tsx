58ef893300dcf3b793023d2770918ba6ca7aa12c
gomestzx <felipematheusdev@gmail.com>
2024-09-09 15:16:35 -0300

    refactor: layout style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,8 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Sections/Footer/Footer";
 import { useRouter } from "next/router";
+import { useTheme } from "@/context/ThemeContext";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,9 +11,11 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   const router = useRouter();
   const currentPath = router.asPath;
+  const { theme } = useTheme()
+
   return (
     <>
-      <div className={`lg:flex items-center justify-center ${currentPath.split("/")[1] !== 'tailwind-components' && 'bg-blured'}   dark:bg-black bg-center bg-no-repeat dark:bg-unset dark:bg-repeat`}>
+      <div className={`lg:flex items-center justify-center ${currentPath === '/' || theme === 'dark' ? 'bg-blured' : 'bg-white'}   dark:bg-black bg-center bg-no-repeat dark:bg-unset dark:bg-repeat`}>
         <div className="lg:max-w-[100rem] min-h-screen min-w-full">
           <Navbar />
           <div className="min-h-screen min-w-full">{children}</div>
--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -5,7 +5,7 @@ const Textarea = (props: ITextarea) => {
     return (
         <>
             <textarea
-                className=' w-full h-72 p-4 font-manrope dark:bg-mainDark dark:border dark:border-gray-600 dark:text-white'
+                className=' w-full h-72 p-4 font-manrope dark:bg-mainDark border rounded-md border-gray-300 dark:border-gray-600 dark:text-white'
                 placeholder={props.placeholder}
                 value={props.value}
                 onChange={props.onChange}
