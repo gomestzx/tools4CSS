aa0cd4e088c582de534a0f1cb1c45e5fb723888d
gomestzx <felipematheusdev@gmail.com>
2024-10-03 14:57:27 -0300

    fix: card and banner styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -44,7 +44,7 @@ const Card = (props: ICard) => {
         <div className="w-full">
 
           <div
-            className={` ${styles.card} border border-slate-300 dark:border-slate-600 dark:hover:border-slate-500 rounded-md shadow-sm bg-custom-gray-main dark:bg-mainDark mx-2 md:mx-0 lg:m-auto relative cursor-pointer h-48 p-4`}
+            className={` ${styles.card} border border-slate-300 dark:border-slate-600 dark:hover:border-slate-500 rounded-md shadow-sm bg-gray-50 dark:bg-mainDark mx-2 md:mx-0 lg:m-auto relative cursor-pointer h-48 p-4`}
           >
             <Image src={props.img ?? ''} width={50} height={50} />
 
