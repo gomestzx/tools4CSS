25517b4b20fb7cc2eb7e34328bf8fd400ea9f3d5
gomestzx <felipematheusdev@gmail.com>
2024-10-09 16:02:18 -0300

    feat: add color in neumorphism

--- src/pages/neumorphism/index.tsx
+++ src/pages/neumorphism/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useControls } from "../../hooks/useControls";
+import React, { useState, useEffect } from "react";
 import CopyButton from "../../components/CopyButton/CopyButton";
 import { darcula } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 import { LightAsync as SyntaxHighlighter } from "react-syntax-highlighter";
@@ -11,19 +10,84 @@ import ControlsContainer from "../../components/ControlsContainer/ControlsContai
 import CustomSlider from "../../components/CustomSlider/CustomSlider";
 import Title from "../../components/Title/Title";
 import Info from "../../components/Info/Info";
+import ColorPicker from "@/components/ColorPicker/ColorPicker";
 
-const Neumorphism = () => {
-  const { blur, setBlur, borderRadius, distance, setDistance } = useControls();
+const Neumorphism: React.FC = () => {
+  const [blur, setBlur] = useState<number>(34);
+  const [borderRadius, setBorderRadius] = useState<number>(20);
+  const [distance, setDistance] = useState<number>(10);
+  const [color, setColor] = useState<string>("#f0f0f0");
+
+  // Ensure that the component only renders on the client side
+  const [mounted, setMounted] = useState(false);
+  
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const { isFavorited, handleFavorite } = useFavoriteTool(
     "Neumorphism Generator"
   );
 
+  const lightenColor = (hex: string, percent: number): string => {
+    hex = hex.replace("#", "");
+
+    let r = parseInt(hex.substring(0, 2), 16);
+    let g = parseInt(hex.substring(2, 4), 16);
+    let b = parseInt(hex.substring(4, 6), 16);
+
+    r = Math.min(255, Math.floor(r + (255 - r) * (percent / 100)));
+    g = Math.min(255, Math.floor(g + (255 - g) * (percent / 100)));
+    b = Math.min(255, Math.floor(b + (255 - b) * (percent / 100)));
+
+    const newHex =
+      "#" +
+      [r, g, b]
+        .map((x) => {
+          const hexPart = x.toString(16);
+          return hexPart.length === 1 ? "0" + hexPart : hexPart;
+        })
+        .join("");
+    return newHex;
+  };
+
+  const darkenColor = (hex: string, percent: number): string => {
+    hex = hex.replace("#", "");
+
+    let r = parseInt(hex.substring(0, 2), 16);
+    let g = parseInt(hex.substring(2, 4), 16);
+    let b = parseInt(hex.substring(4, 6), 16);
+
+    r = Math.max(0, Math.floor(r - r * (percent / 100)));
+    g = Math.max(0, Math.floor(g - g * (percent / 100)));
+    b = Math.max(0, Math.floor(b - b * (percent / 100)));
+
+    const newHex =
+      "#" +
+      [r, g, b]
+        .map((x) => {
+          const hexPart = x.toString(16);
+          return hexPart.length === 1 ? "0" + hexPart : hexPart;
+        })
+        .join("");
+    return newHex;
+  };
+
+  const lighterColor = lightenColor(color, 20);
+  const darkerColor = darkenColor(color, 20);
+
+  const boxShadowStyle = `${distance}px ${distance}px ${blur}px ${darkerColor}, -${distance}px -${distance}px ${blur}px ${lighterColor}`;
+
+  // Render null on the server to avoid mismatch issues, only render on the client
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <>
       <SEO title="Neumorphism" />
       <div className="w-full lg:w-4/6 mx-auto font-medium bg-transparent">
-        <div className=" flex flex-row justify-between items-center mx-4 lg:mx-0">
+        <div className="flex flex-row justify-between items-center mx-4 lg:mx-0">
           <Breadcrumb
             links={[{ href: "/", label: "Home" }, { label: "Neumorphism" }]}
           />
@@ -32,39 +96,46 @@ const Neumorphism = () => {
             handleFavorite={handleFavorite}
           />
         </div>
+
         <Title
           title="Neumorphism CSS Generator"
           info="Effortlessly create soft, subtle 3D effects with our Neumorphism CSS Generator"
         />
-        <div className="mt-4 flex justify-center bg-white py-6 rounded-lg">
+
+        <div
+          style={{ backgroundColor: color }}
+          className="mt-4 flex justify-center py-20 rounded-2xl"
+        >
           <div
             style={{
               width: "240px",
               height: "240px",
               borderRadius: `${borderRadius}px`,
-              boxShadow: `${distance}px ${distance}px ${blur}px #b2b8c9, -${distance}px -${distance}px ${blur}px #f0f8ff`,
+              boxShadow: boxShadowStyle,
+              backgroundColor: color,
             }}
           ></div>
         </div>
+
         <ControlsContainer>
+          <ColorPicker value={color} onChange={(color) => setColor(color)} />
           <div className="flex items-center justify-center gap-2">
-            <span>Direction:</span>
+            <span>Distance:</span>
             <CustomSlider
               style={{ width: 200, marginLeft: 20, marginRight: 20 }}
               value={distance}
-              onChange={(_e: any, value: number) =>
-                setDistance(value as number)
-              }
+              onChange={(_e: any, value: number) => setDistance(value)}
               defaultValue={30}
               max={60}
             />
           </div>
+
           <div className="flex items-center justify-center gap-2">
             <span>Blur:</span>
             <CustomSlider
               style={{ width: 200, marginLeft: 20, marginRight: 20 }}
               value={blur}
-              onChange={(_e: any, value: number) => setBlur(value as number)}
+              onChange={(_e: any, value: number) => setBlur(value)}
               defaultValue={30}
             />
           </div>
@@ -72,34 +143,38 @@ const Neumorphism = () => {
 
         <div className="w-auto md:w-full bg-slate-600 mt-4 mx-4 lg:mx-0">
           <SyntaxHighlighter language="css" style={darcula}>
-            {`box-shadow: ${distance}px ${distance}px ${blur}px #b2b8c9, -${distance}px -${distance}px ${blur}px #f0f8ff;\n-webkit-box-shadow: ${distance}px ${distance}px ${blur}px #b2b8c9, -${distance}px -${distance}px ${blur}px #f0f8ff;`}
+            {`background-color: ${color};
+box-shadow: ${boxShadowStyle};
+-webkit-box-shadow: ${boxShadowStyle};`}
           </SyntaxHighlighter>
         </div>
 
         <div className="mx-4 lg:mx-0">
           <CopyButton
-            textToCopy={`box-shadow: ${distance}px ${distance}px ${blur}px #b2b8c9, -${distance}px -${distance}px ${blur}px #f0f8ff;\n-webkit-box-shadow: ${distance}px ${distance}px ${blur}px #b2b8c9, -${distance}px -${distance}px ${blur}px #f0f8ff;`}
+            textToCopy={`background-color: ${color};
+box-shadow: ${boxShadowStyle};
+-webkit-box-shadow: ${boxShadowStyle};`}
           />
         </div>
+
         <Info
           title="What is Neumorphism?"
           paragraph={
             <>
-              {" "}
               <p>
                 Neumorphism is a design trend that has gained popularity in user
                 interface (UI) design. It represents a blend of background
-                colors, soft shadow, and smooth shapes to emulate physicality
+                colors, soft shadows, and smooth shapes to emulate physicality
                 through digital elements. Unlike traditional skeuomorphism,
                 which aims to mimic real-world textures and objects closely,
                 neumorphism focuses on subtle, soft, and almost tactile surfaces
-                that appear to extrude from or recede into the background.{" "}
+                that appear to extrude from or recede into the background.
               </p>
               <br />
               <p>
                 The Neumorphism Generator is a web development tool designed to
                 assist in crafting user interface (UI) elements featuring the
-                neumorphic design style, using Cascading Style Sheets (CSS)
+                neumorphic design style, using Cascading Style Sheets (CSS).
               </p>
             </>
           }
