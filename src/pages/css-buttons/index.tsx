e4b37ac4fa1e0c4abb1f3e38faad44e8e2883223
gomestzx <felipematheusdev@gmail.com>
2025-01-12 17:45:59 -0300

    refactor: css button compnent styles

--- src/pages/css-buttons/index.tsx
+++ src/pages/css-buttons/index.tsx
@@ -5,12 +5,14 @@ import { Breadcrumb } from "../../components/Breadcrumb/Breadcrumb";
 import { SEO } from "../../components/SEO";
 import FavoriteButton from "../../components/FavoriteButton/FavoriteButton";
 import { useFavoriteTool } from "../../hooks/useFavoriteTool";
-import { MdClose, MdCode } from "react-icons/md";
+import { MdClose, MdCode, MdFavoriteBorder } from "react-icons/md";
 import Title from "../../components/Title/Title";
 import Info from "../../components/Info/Info";
 import CopyButton from "../../components/CopyButton/CopyButton";
 import { buttonsGenerator } from "@/constants/buttonsGenerator";
 import { css as beautifyCSS } from 'js-beautify';
+import { TbSourceCode } from "react-icons/tb";
+import { IoIosHeartEmpty } from "react-icons/io";
 
 export interface IButtonGenerator {
   id: number;
@@ -76,14 +78,24 @@ const ButtonsGenerator: React.FC = () => {
               <button className={`${button.className} font-lexend font-normal`}>
                 {button.text}
               </button>
-              <div className="p-2 absolute bottom-0 right-0">
+              <div className="p-2 absolute top-0 right-0">
                 <button
-                  className="bg-dark-100 dark:bg-black py-1 px-4 rounded-lg text-white flex gap-2 justify-center items-center font-lexend font-normal"
+                  className="bg-gray-50 border border-slate-300 text-black dark:bg-dark-100 dark:border-slate-600 dark:text-white py-2 px-2 rounded-full flex gap-2 justify-center items-center font-lexend font-normal text-sm"
                   onClick={() => handleButtonClick(button)}
                 >
-                  Get the code <MdCode />
+                 <MdFavoriteBorder size={16} className="  text-black dark:text-white" />
                 </button>
               </div>
+              <div className="p-2 absolute bottom-0 right-0">
+                <button
+                  className="bg-gray-50 border border-slate-300 text-black dark:bg-dark-100 dark:border-slate-600 dark:text-white py-2 px-4 rounded-full flex gap-2 justify-center items-center font-lexend font-normal text-sm"
+                  onClick={() => handleButtonClick(button)}
+                >
+                  <svg className="w-[18px] h-[18px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M20 13.5c0 1.395 0 2.092-.138 2.667a5 5 0 0 1-3.695 3.695C15.592 20 14.894 20 13.5 20H12c-2.8 0-4.2 0-5.27-.545a5 5 0 0 1-2.185-2.185C4 16.2 4 14.8 4 12v-.5c0-2.33 0-3.495.38-4.413A5 5 0 0 1 7.088 4.38c.776-.322 1.73-.372 3.413-.38m9.26 5.454c.262-1.633.31-3.285.142-4.914a.495.495 0 0 0-.142-.3m0 0a.496.496 0 0 0-.301-.143 18.815 18.815 0 0 0-4.913.142m5.214 0L10 14"></path></svg>
+                  Get the code
+                </button>
+              </div>
+
             </div>
           ))}
           <Info
@@ -96,13 +108,14 @@ const ButtonsGenerator: React.FC = () => {
         <div
           className={`fixed inset-0 z-50 flex items-center justify-center ${fullScreen ? "block" : "hidden"
             }`}
-        >
+        > 
           <div
             className="fixed inset-0 bg-black opacity-50"
             onClick={handleCloseModal}
           ></div>
           <div className="bg-gray-50 dark:bg-black border-4 border-slate-300 dark:border-dark-100 rounded-lg p-4 md:p-4 relative z-10 w-full max-w-lg mx-4 md:mx-auto">
-            <div className="mb-4 flex justify-end">
+            <div className="mb-4 flex justify-between items-center">
+            <span className="text-black dark:text-white font-lexend">{selectedButton.text}</span>
               <button
                 className="dark:text-white text-black bg-gray-200 dark:bg-dark-100 p-2 rounded-full"
                 onClick={handleCloseModal}
@@ -117,13 +130,13 @@ const ButtonsGenerator: React.FC = () => {
                 </button>
               </div>
 
-              <div className="w-full mt-4 max-h-60 overflow-auto">
+              <div className="w-full mt-4 max-h-60 overflow-auto rounded-lg">
                 <SyntaxHighlighter language="css" style={darcula}>
                   {selectedButton.css}
                 </SyntaxHighlighter>
               </div>
               <div className="mt-2 w-full">
-                <CopyButton textToCopy={selectedButton.css ?? ""} />
+                <CopyButton className="rounded-lg" textToCopy={selectedButton.css ?? ""} />
               </div>
             </div>
           </div>
